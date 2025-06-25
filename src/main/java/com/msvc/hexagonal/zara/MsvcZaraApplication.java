7b0a3cddc57beec789ba56b2f4ca66a709be6e2e
unknown <Edwin Capillo>
2025-06-24 20:53:01 -0500

    TK-zara: Iniciando el proyecto

---
+++ src/main/java/com/msvc/hexagonal/zara/MsvcZaraApplication.java
@@ -0,0 +1,13 @@
+package com.msvc.hexagonal.zara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcZaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcZaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msvc/hexagonal/zara/MsvcZaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.hexagonal.zara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcZaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
