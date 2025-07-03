b8c4072491af11efbb8bb97278f9e4109805f442
unknown <Edwin Capillo>
2025-07-03 13:17:24 -0500

    TK-V2: optimizacion de prueba de integración y checklyst

--- src/test/java/com/msvc/hexagonal/zara/infrastructure/controller/PriceControllerIntegrationTest.java
+++ src/test/java/com/msvc/hexagonal/zara/infrastructure/controller/PriceControllerIntegrationTest.java
@@ -1,14 +1,13 @@
 package com.msvc.hexagonal.zara.infrastructure.controller;
 
-import com.msvc.hexagonal.zara.infrastructure.controller.dto.PriceRequest;
-import com.msvc.hexagonal.zara.infrastructure.controller.dto.PriceSummaryResponse;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -16,99 +15,43 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test") // Use a specific profile for testing
+@ActiveProfiles("test")
 class PriceControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void testGetPriceAt10AM14th() throws Exception {
-        String requestBody = """
+    @ParameterizedTest
+    // en CsvSource se especifican que los valores de entrada son las primeras 3 columnas y
+    // las siguientes columnas representan los valores esperados
+    @CsvSource({
+            "2020-06-14-10:00:00, 35455, 1, 35455, 1, 1, 2020-06-14-00:00:00, 2020-12-31-23:59:59, 35.50, EUR",
+            "2020-06-14-16:00:00, 35455, 1, 35455, 1, 2, 2020-06-14-15:00:00, 2020-06-14-18:30:00, 25.45, EUR",
+            "2020-06-14-21:00:00, 35455, 1, 35455, 1, 1, 2020-06-14-00:00:00, 2020-12-31-23:59:59, 35.50, EUR",
+            "2020-06-15-10:00:00, 35455, 1, 35455, 1, 3, 2020-06-15-00:00:00, 2020-06-15-11:00:00, 30.50, EUR",
+            "2020-06-16-21:00:00, 35455, 1, 35455, 1, 4, 2020-06-15-16:00:00, 2020-12-31-23:59:59, 38.95, EUR"
+    })
+    void testGetPrice(String inputDate, long productId, long brandId, long expectedProductId, long expectedBrandId,
+                      int expectedPriceList, String expectedStartDate, String expectedEndDate, double expectedPrice,
+                      String expectedCurrency) throws Exception {
+        String requestBody = String.format("""
                 {
-                    "inputDate": "2020-10-14-10:00:00",
-                    "brandId": 1,
-                    "productId": 35455
+                    "inputDate": "%s",
+                    "productId": %d,
+                    "brandId": %d
                 }
-                """;
+                """, inputDate, productId, brandId);
 
         mockMvc.perform(post("/api/price")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestBody))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.productId").value(35455))
-                .andExpect(jsonPath("$.brandId").value(1));
-    }
-
-    @Test
-    void testGetPriceAt4PM14th() throws Exception {
-        String requestBody = """
-                {
-                    "inputDate": "2020-10-14-16:00:00",
-                    "productId": 35455,
-                    "brandId": 1
-                }
-                """;
-
-        mockMvc.perform(post("/api/price")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.productId").value(35455))
-                .andExpect(jsonPath("$.brandId").value(1));
-    }
-
-    @Test
-    void testGetPriceAt9PM14th() throws Exception {
-        String requestBody = """
-                {
-                    "inputDate": "2020-10-14-21:00:00",
-                    "productId": 35455,
-                    "brandId": 1
-                }
-                """;
-
-        mockMvc.perform(post("/api/price")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.productId").value(35455))
-                .andExpect(jsonPath("$.brandId").value(1));
-    }
-
-    @Test
-    void testGetPriceAt10AM15th() throws Exception {
-        String requestBody = """
-                {
-                    "inputDate": "2020-10-15-10:00:00",
-                    "productId": 35455,
-                    "brandId": 1
-                }
-                """;
-
-        mockMvc.perform(post("/api/price")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.productId").value(35455))
-                .andExpect(jsonPath("$.brandId").value(1));
-    }
-
-    @Test
-    void testGetPriceAt9PM16th() throws Exception {
-        String requestBody = """
-                {
-                    "inputDate": "2020-10-16-21:00:00",
-                    "productId": 35455,
-                    "brandId": 1
-                }
-                """;
-
-        mockMvc.perform(post("/api/price")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.productId").value(35455))
-                .andExpect(jsonPath("$.brandId").value(1));
+                .andExpect(jsonPath("$.productId").value(expectedProductId))
+                .andExpect(jsonPath("$.brandId").value(expectedBrandId))
+                .andExpect(jsonPath("$.priceList").value(expectedPriceList))
+                .andExpect(jsonPath("$.startDate").value(expectedStartDate))
+                .andExpect(jsonPath("$.endDate").value(expectedEndDate))
+                .andExpect(jsonPath("$.price").value(expectedPrice))
+                .andExpect(jsonPath("$.currency").value(expectedCurrency));
     }
 }
